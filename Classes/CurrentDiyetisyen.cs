35009c6e1f9d9e12a0168a0ee9fcfd4aa145e843
Atakan Him <atakanhim@gmail.com>
2021-05-27 00:06:56 +0300

    updated
    new

---
+++ Classes/CurrentDiyetisyen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diyetisyenproje.Classes
+{
+    class CurrentDiyetisyen
+    {
+        public string DiyetisyenTc { get; set; }
+        public string DiyetisyenPassword { get; set; }
+        public string DiyetisyenAd { get; set; }
+        public string DiyetisyenSoyad { get; set; }
+        public string DiyetisyenTelefon { get; set; }
+        public string DiyetisyenAdres { get; set; }
+        public String DiyetisyenIseBaslamaTarihi { get; set; }
+        public CurrentDiyetisyen()
+        {
+
+        }
+        public void CurrentDiyetisyenEkle(string diyeTc, string diyePassword, string diyeAd, string diyeSoyad, string diyeTelefon, string diyeAdres, String tarih)
+        {
+            this.DiyetisyenTc = diyeTc;
+            this.DiyetisyenPassword = diyePassword;
+            this.DiyetisyenAd = diyeAd;
+            this.DiyetisyenSoyad = diyeSoyad;
+            this.DiyetisyenTelefon = diyeTelefon;
+            this.DiyetisyenAdres = diyeAdres;
+            this.DiyetisyenIseBaslamaTarihi = tarih;
+          
+        }
+    }
+}
---
+++ Screens/DiyetisyenScreen.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace diyetisyenproje
+{
+    partial class DiyetisyenScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiyetisyenScreen));
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.BackColor = System.Drawing.Color.Transparent;
+            this.exit_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exit_btn.BackgroundImage")));
+            this.exit_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exit_btn.FlatAppearance.BorderSize = 0;
+            this.exit_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.exit_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.exit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit_btn.Location = new System.Drawing.Point(1176, 12);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(44, 37);
+            this.exit_btn.TabIndex = 63;
+            this.exit_btn.UseVisualStyleBackColor = false;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // back_btn
+            // 
+            this.back_btn.BackColor = System.Drawing.Color.Transparent;
+            this.back_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("back_btn.BackgroundImage")));
+            this.back_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.back_btn.FlatAppearance.BorderSize = 0;
+            this.back_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.back_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.back_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back_btn.Location = new System.Drawing.Point(1126, 12);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(44, 37);
+            this.back_btn.TabIndex = 62;
+            this.back_btn.UseVisualStyleBackColor = false;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(649, 303);
+            this.dataGridView1.TabIndex = 64;
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Location = new System.Drawing.Point(21, 24);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(35, 13);
+            this.name_label.TabIndex = 65;
+            this.name_label.Text = "label1";
+            // 
+            // DiyetisyenScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 606);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.back_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DiyetisyenScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DiyetisyenScreen";
+            this.Load += new System.EventHandler(this.DiyetisyenScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label name_label;
+    }
+}
