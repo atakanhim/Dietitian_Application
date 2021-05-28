d41ad7c7b76f23e9a50d4a4730388d28e64fbf5b
Atakan Him <atakanhim@gmail.com>
2021-05-28 21:56:12 +0300

    add
    add

---
+++ Screens/HastaTedaviScreen.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diyetisyenproje.Screens
+{
+    public partial class HastaTedaviScreen : Form
+    {
+        public HastaTedaviScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_btn_Click(object sender, EventArgs e) => Singleton.Instance.ChangeScreen(this, Singleton.Instance.dScreen);
+
+        private void HastaTedaviScreen_Load(object sender, EventArgs e)
+        {
+            OnLoad();
+        }
+        public void OnLoad()
+        {
+            diyeteklegrpbox.Visible = false;
+            hasta_bilgileri_lbl.Text = Singleton.Instance.secilenHastabilgileri.Hastabilgileri;
+            Singleton.Instance.islem.ComboBoxDoldur(HastaSikayetleri_combo,"SELECT * from hastaliklar","hastalikAd","Yeni Hastalik Ekle");
+            Singleton.Instance.islem.ComboBoxDoldur(DiyetTuruCombo_combo, "SELECT * from diyetler", "diyetAd","Yeni Diyet Ekle");
+
+            if (Singleton.Instance.secilenHastabilgileri.HastaHastalığı != "Şikayet Belirtilmedi")
+            {
+                sikayetlistele_label.Visible = false;
+                HastaSikayetleri_combo.Visible = false;
+                DiyetTuruCombo_combo.Visible = true;//hasta şikayet belirtmemişse biz şikayeti belirtiyosak diyet henüz aktif deigl
+                diyetsec_label.Visible = true;
+                diyet_bilgisi.Visible = true;
+            }       
+            else
+            {             
+                sikayetlistele_label.Visible = true;
+                HastaSikayetleri_combo.Visible = true;
+                diyet_bilgisi.Visible = false;
+                DiyetTuruCombo_combo.Visible = false;
+                diyetsec_label.Visible = false;
+                
+            }
+
+            hasta_sikayeti.Text = "Hasta şikayeti: " + Singleton.Instance.secilenHastabilgileri.HastaHastalığı;
+            diyet_bilgisi.Text= "Diyet Bilgisi : " + Singleton.Instance.secilenHastabilgileri.HastaDiyetTuru;
+        }
+
+        private void DiyetTuruCombo_combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(DiyetTuruCombo_combo.Text== "Yeni Diyet Ekle" && HastaSikayetleri_combo.Text!="Yeni Hastalik Ekle")
+            {
+                diyeteklegrpbox.Visible = true;
+                rapor_olustur.Visible = false;
+            }
+            else
+            {
+                diyeteklegrpbox.Visible = false;
+                rapor_olustur.Visible = true;
+            }
+               
+                    
+        }
+        private void HastaSikayetleri_combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (HastaSikayetleri_combo.Text == "Yeni Hastalik Ekle")
+            {
+                hastalikEkleGrpbox.Visible = true;
+                diyeteklegrpbox.Visible = false;
+                DiyetTuruCombo_combo.Visible = false;
+                diyetsec_label.Visible = false;
+                diyet_bilgisi.Visible = false;
+                rapor_olustur.Visible = false;
+            }
+            else
+            {
+                hastalikEkleGrpbox.Visible = false;
+                DiyetTuruCombo_combo.Visible = true;           
+                diyetsec_label.Visible = true;
+                diyet_bilgisi.Visible = true;
+            }
+        }
+
+      private void HastalikEkle_btn_Click(object sender, EventArgs e) =>  Singleton.Instance.islem.HastalikEkle(hastalikAdi_txt.Text, hastaaciklama_txt.Text,this);
+
+        private void DiyetEkle_btn_Click(object sender, EventArgs e) => Singleton.Instance.islem.DiyetEkle(diyetadı_txt.Text,
+            pazartesi_txt.Text, sali_txt.Text, carsanba_txt.Text, persenbe_txt.Text, cuma_txt.Text, cumartesi_txt.Text, pazar_txt.Text, this);
+
+        private void rapor_olustur_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
